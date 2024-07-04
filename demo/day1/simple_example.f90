230423d76646304f4870e057ce7681a0d25d99b2
Pavanakumar Mohanamuraly <mpkumar@cerfacs.fr>
2024-07-04 20:40:54 +0200

    Compiling example

--- demo/day1/simple_example.f90
+++ demo/day1/simple_example.f90
@@ -7,10 +7,10 @@ end subroutine f
 
 
 subroutine f1(y, x)
-  real, intent(out) :: u
-  real, intent(in)  :: x(3), y(2)
-  real :: w, v
-  real, parameter :: piAccurate = 3.14159265358979320
+  real, intent(out) :: y(2)
+  real, intent(in)  :: x(3)
+  real :: w, v, u
+  real, parameter :: pi = 3.14159265358979320
   u = 3*x(1) + 2*x(2) + x(3)
   v = pi * sin(u)
   w = pi * cos(u) * v

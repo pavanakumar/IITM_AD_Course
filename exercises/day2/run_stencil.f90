8ff632a1d5196dbd6d1b93e205423b3648c38d24
Pavanakumar Mohanamuraly <mpkumar@cerfacs.fr>
2024-07-08 08:15:37 +0200

    Latest stencil colouring code

--- exercises/day2/run_stencil.f90
+++ exercises/day2/run_stencil.f90
@@ -3,16 +3,82 @@ program run_stencil
   integer, parameter :: n = 5
   real :: u(n), ud(n)
   real :: res(n), resd(n)
-  integer :: i
+  real :: jacob(n, n)
+  real :: jacob_colour(n, 3)
+  integer :: i, j
+  integer :: icolour
 
-  u = 1.0
-  res = 0.0
   do i = 1, n
+    u = 1.0
+    res = 0.0
     ud = 0.0
+    resd = 0.0
     ud(i) = 1.0
     call stencil_d(n, (/ 1.0, -2.0, 3.0 /), u, ud, res, resd)
-    write(*,*) resd
+    ! Construct one column at a time
+    jacob(i, 1:n) = resd
   end do
- 
+
+  write(*,*)
+  write(*, *) "Forward mode Jacobian (A x)"
+  write(*, *) "--------------------------"
+  do i = 1, n
+   do j = 1, n
+     write(*, "(SP, F8.2,A)", advance='no') jacob(j, i) 
+   end do
+   write(*,*)
+  end do 
+
+  write(*,*) 
+  write(*, *) "Reverse mode Jacobian (A^T x)"
+  write(*, *) "-----------------------------"
+  do i = 1, n
+   do j = 1, n
+     write(*, "(SP, F8.2,A)", advance='no') jacob(i, j) 
+   end do
+   write(*,*)
+  end do 
+
+  ! Seed based on 3 colouring
+  write(*,*)
+  write(*,*) "Seed vectors"
+  write(*,*) "------------"
+  do icolour = 1, 3
+    ud = 0.0
+    do i = icolour, n, 3
+      ud(i) = 1.0
+    end do
+    write(*,"(A,I1,A)", advance='no') "   Seed (", icolour, ")"
+    do i = 1, n
+      write(*, "(SP, F8.2,A)", advance='no') ud(i)
+    end do
+    write(*,*)
+  end do 
+
+  ! Jacobian colouring (3 colour)
+  do icolour = 1, 3
+    u = 1.0
+    res = 0.0
+    ud = 0.0
+    resd = 0.0
+    ! Seed based on 3 colouring
+    do i = icolour, n, 3
+      ud(i) = 1.0
+    end do
+    call stencil_d(n, (/ 1.0, -2.0, 3.0 /), u, ud, res, resd)
+    ! Construct one colour at a time
+    jacob_colour(1:n, icolour) = resd(1:n)
+  end do
+
+  write(*,*) 
+  write(*,*) "Jacobian Coloured"
+  write(*,*) "-----------------"
+  do i = 1, n
+    do icolour = 1, 3
+      write(*, "(SP, F8.2,A)", advance='no') jacob_colour(i, icolour)
+    end do
+    write(*,*)
+  end do
+
 end program run_stencil
 
--- exercises/day2/stencil.f90
+++ exercises/day2/stencil.f90
@@ -7,10 +7,10 @@ subroutine stencil(n, coeff, u, res)
   integer :: i
 
   ! BC left
-  res(1) = coeff(1) * u(n) + coeff(2) * u(1) + coeff(3) * u(2)
+  res(1) = coeff(2) * u(1) + coeff(3) * u(2)
 
   ! BC right
-  res(n) = coeff(1) * u(n - 1) + coeff(2) * u(n) + coeff(3) * u(1)
+  res(n) = coeff(1) * u(n - 1) + coeff(2) * u(n)
 
   do i = 2, n - 1
     res(i) = coeff(1) * u(i - 1) + coeff(2) * u(i) + coeff(3) * u(i + 1)
